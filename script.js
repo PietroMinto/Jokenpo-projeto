5a50f513a77a05a970afc8070c1101bce2a7b21d
PietroMinto <pietrofulanminto@gmail.com>
2024-08-12 16:02:34 -0300

    primeiro commit, projeto de pedra papel tesoura

---
+++ script.js
@@ -0,0 +1,39 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const MachineScore = document.querySelector('#machine-score')
+
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const PlayHuman = (HumanChoice) => {
+
+    PlayTheGame(HumanChoice, PlayMachine())
+}
+const PlayMachine = (MachineChoice) => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+}
+
+const PlayTheGame = (human, machine) => {
+
+    console.log('Humano: ' + human + " Maquina: " + machine)
+
+    if(human === machine) {
+        result.innerHTML = "Deu Empate!"
+    } else if ( 
+     ( human === 'paper' && machine === 'rock') ||
+     (human === 'rock' && machine === 'scissors') || 
+     (human === 'scissors' && machine === 'paper')
+      ) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+         result.innerHTML = "Você ganhou!"
+    } else {
+        machineScoreNumber++
+        MachineScore.innerHTML = machineScoreNumber
+          result.innerHTML = "Você perdeu para a Alexa!"
+    }
+}
